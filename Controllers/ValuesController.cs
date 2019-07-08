e0612e2f056f4d86c1df54dfa75556c33f097970
KERNGRUPPE\nk.CHRIS1337 <christopher.wojciech@netzkern.de>
2019-07-08 21:45:12 +0200

    Initial Commit Added Prototype code

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SiennaAidHub.Models;
+using SiennaAidHub.Services;
+
+namespace SiennaAidHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+	private IncomingService _incomingService;
+
+	public ValuesController(IncomingService incomingService)
+	{
+	    _incomingService = incomingService;
+	}
+
+	// GET api/values
+	[HttpGet]
+	public ActionResult<IEnumerable<string>> Get()
+	{
+	    return new string[] { "value1", "value2" };
+	}
+
+	// GET api/values/5
+	[HttpGet("{id}")]
+	public ActionResult<string> Get(int id)
+	{
+	    return "value";
+	}
+
+	// POST api/values
+	[HttpPost]
+	public ActionResult<string> Post([FromBody] Incoming value)
+	{
+	    Console.WriteLine("INCOMING: " + value.Value);
+
+	    // TODO [CW]
+	    // var created = _incomingService.Create(value);
+	    // Console.WriteLine("CREATED: " + created.Id);
+
+	    return ":-)";
+	}
+
+	// PUT api/values/5
+	[HttpPut("{id}")]
+	public void Put(int id, [FromBody] string value)
+	{
+	}
+
+	// DELETE api/values/5
+	[HttpDelete("{id}")]
+	public void Delete(int id)
+	{
+	}
+    }
+}
---
+++ Models/ISiennaDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiennaAidHub.Models
+{
+    public interface ISiennaDatabaseSettings
+    {
+	string SiennaCollectionName { get; set; }
+	string ConnectionString { get; set; }
+	string DatabaseName { get; set; }
+    }
+}
